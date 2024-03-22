1c7bb611e7b1a7a21201ac19a8d261eda4435c66
Mickaël <mickael.durand@outlook.com>
2024-03-22 16:16:36 +0100

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,9 @@
 //On détermine le point central de la map (Ici, Saint-Etienne long 45.439 lat 4.387)
 let map = L.map('map').setView([45.439, 4.387], 12);
 //On met en place le fond de la map
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 //On crée un icone marqueur pour les boutiques M'S
 var msIcon = L.icon({
